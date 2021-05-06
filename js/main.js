dfa8a676d8993352e150d2e34ada34b48a27adce
minhbao2103 <trinhminhbao21031996@gmail.com>
2021-05-06 20:39:26 +0700

    lan dau commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+//vonglap for (khai bao bien(vitribatdau la 0) , cau dieu kien dung`, tang gia tri)
+
+//for(let i=0; i<3; i=i+1) {
+//console.log(a[i])
+//}
+
+//i = 0 ; 0 < 3
+
+//function hinhanh (name,price) {
+    //document.getElementById("container").innerHTML +=`
+    //<div class="product">
+    //<h4>` + name + ` giá `+ price +`</h4>
+   
+    //</div>
+    //`
+//}
+//let tensanpham=["áo","nón","quần","túi"]
+//let giatien= ["20.000 vnd","50.000 vnd","40.000 vnd","20.000 vnd"]
+//for (let i=0; i<4; i=i+1) {
+  //  hinhanh(tensanpham[i],giatien[i])
+//}
+
+//let a = [1,2,3,4]
+//let tong =0
+//for ( let i = 0 ; i<4; i= i+1 ){
+  //tong= tong + a[i]
+    //console.log(tong)
+//}
+
+
+//let a = [0,3,11,5,8,52,6,7,8,91]
+
+//for (let i=0; i<10;i=i+1){
+   
+  //if(a[i]%2!=0) {
+    //console.log(a[i])
+  //}
+  
+  cach 2
+let danhsachsanpham =[
+  ["non",1000,"hinhanh"]
+  ["quan",1000,"hinhanh"]
+  ["ao",1000,"hinhanh"]
+]
+muon in ra danh sach san pham thu 0 thi bam 
+danhsachsanpham[0] enter la ra
+
+muon in ra gia cua san pham 0 thi bam
+danhsachsanpham[0][1] (1 o day la thu tu trong mang)
+
+
+mang 3 chiều la 
+let test = ["NON",10000,4,"...",["nam","nu"]]
+
+
+
+bai hoc ngay 12/4
+let str = "hello" (str la string)
+str[0] la se ra ki tu dau tien la h
+str.length enter la se ra dc chieu dai cua mảng 
+hoặc là "hello world".lenght enter thì nó sẽ hiện ra chiều dài của mảng
+"xin chào mọi người".split(" muốn tách chữ nào thì bấm vào")
+"xin chào mọi người".split("chào") 
+split la sau phan tu quy dinh thì cắt ra đưa vào trong mảng   
+mảng sẽ hiển thị là ["xin" , "chào mọi người"]
+"xin chào".toUpperCase() là in hoa chữ "xin chào" , sẽ hiển thị là "XIN CHÀO"
+"XIN CHÀO".tolowercase() là in thường chữ "XIN CHÀO", sẽ hiển thị là " xin chào"
+# nhập vào một chuỗi , xuất ra từng ký tự của chuỗi 
+"helloworld".slice (5) slice la cắt chuỗi, slice là cắt từ đâu đến đâu .slice(start,end)
+thi no se in ra world
